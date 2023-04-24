f73c59b5c683f0d3ae4bef9283db33630ac41b08
Gnzrg <baterdeneganzorig959@gmail.com>
2023-04-24 09:44:42 +0800

    search

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,16 +1,20 @@
 import React from "react";
 import { SearchBox } from "./SearchBox";
+import { useContext } from "react";
+import { SearchContext } from "@/context/searchTextContext";
 
 export const Search = (props: {
   handleChange: React.ChangeEventHandler<HTMLInputElement> | undefined;
 }): JSX.Element => {
+  const { search, setSearch } = useContext(SearchContext);
+
   return (
     <div>
       <input
-        className="search-box w-full rounded-md border pt-2 border-black"
+        className="search-box w-full rounded-md border pt-2 border-teal-300 px-8 text-xl text-indigo-500 pb-1"
         type="search"
-        placeholder="search..."
-        onChange={props.handleChange}
+        placeholder="Search..."
+        onChange={(e) => setSearch(e.target.value)}
       />
     </div>
   );
