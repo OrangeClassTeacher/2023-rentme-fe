7488a8039f65ec203afaede2f5477715e767bb90
Gnzrg <baterdeneganzorig959@gmail.com>
2023-04-26 12:23:28 +0800

    a

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import axios from "axios";
-import Link from "react";
+import Link from "next/link";
 interface IUser {
   firstName: string;
   lastName: string;
@@ -50,7 +50,9 @@ export default function Index() {
           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-1/3 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           placeholder="Password"
         />
-        <p className="underline text-sky-500">Бүртгэл үүсгэх</p>
+        <p className="underline text-sky-500">
+          <Link href="/register">Бүртгэл үүсгэх</Link>
+        </p>
         <button
           onClick={login}
           className="text-indigo-500 outline outline-offset-2 outline-blue-500 rounded px-3"
---
+++ src/pages/register/index.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { useState } from "react";
+import axios from "axios";
+import { Iproduct } from "@/interfaces/product";
+import Link from "next/link";
+
+export default function Index() {
+  const [user, setUser] = useState<Iproduct>();
+  const createUser = async () => {
+    axios
+      .post("http://localhost:8000/api/user", user)
+      .then((res) => console.log(res.data.result));
+  };
+  return (
+    <div>
+      <form onSubmit={createUser}>
+        <div>
+          <input
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-1/3 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Email"
+          />
+          <input
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-1/3 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Email"
+          />
+          <input
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-1/3 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Email"
+          />
+          <input
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-1/3 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Email"
+          />
+          <input
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-1/3 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Email"
+          />
+          <input
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-1/3 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Email"
+          />
+          <input
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-1/3 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Email"
+          />
+          <button
+            type={"submit"}
+            className="text-indigo-500 outline outline-offset-2 outline-blue-500 rounded px-3ß"
+          >
+            <Link href="/">Register</Link>
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
