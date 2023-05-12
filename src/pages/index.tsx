72e002a17ffc2fe99f126581caf6335ff7131e90
Gnzrg <baterdeneganzorig959@gmail.com>
2023-05-12 10:01:23 +0800

    main

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,20 @@
 import { Inter } from "next/font/google";
+import { Category } from "../components/landingPage/MCategories";
+import NewsSection from "../components/landingPage/NewsSection";
+import { MCategories } from "../components/landingPage/MCategories";
+import Swiper from "@/components/Home/Swiper";
+import CarouselSection from "@/components/Home/Carousel";
 
-import { NewsSection } from "../components/landingPage/NewsSection";
-// import { MCategories } from "@/components/landingPage/MCategories";
-import { Items } from "@/components/landingPage/Items";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <div>
+      <Category />
+      <CarouselSection />
+      <Swiper />
       <NewsSection />
-      <Items />
+      <MCategories />
     </div>
   );
 }
