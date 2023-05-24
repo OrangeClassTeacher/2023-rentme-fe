fa7c4a0de96afa009d8904a3959269d7a4bfb1f9
Orgil <user@users-MacBook-Pro.local>
2023-05-24 09:47:06 +0800

    load

---
+++ src/components/Loading/Loading.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Loading(): JSX.Element {
+  return (
+    <div className="flex justify-center h-full pt-52">
+      loading.....
+    </div>
+  );
+}
